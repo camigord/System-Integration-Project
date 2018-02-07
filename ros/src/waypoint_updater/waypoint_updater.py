b701ad36f60680fe5ccf86a569de02b0a9026508
Stefano Salati <stef.salati@gmail.com>
2018-02-07 07:28:50 -0800

    Added a couple callbacks and renamed self.waypoints to self.base_waypoints (I was just getting confused with all these variables called waypoints)

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -3,6 +3,7 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
+from std_msgs.msg import Int32, Float32
 
 import math
 import numpy as np
@@ -33,7 +34,8 @@ class WaypointUpdater(object):
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-
+        rospy.Subscriber('/traffic_waypoint',  Int32, self.traffic_cb)
+        rospy.Subscriber('/obstacle_waypoint', Int32, self.obstacle_cb)
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
@@ -46,44 +48,47 @@ class WaypointUpdater(object):
 
     def publish_waypoints(self):
         msg = Lane()
-        msg.header = self.waypoints.header
+        msg.header = self.base_waypoints.header
         msg.waypoints = self.final_waypoints
         self.final_waypoints_pub.publish(msg)
 
+    """
+    Callbacks
+    """
     def pose_cb(self, msg):
         #
         dist_f = lambda a, b: math.sqrt((a.x - b.x)**2 + (a.y - b.y)**2)
         distances = []
-        if self.waypoints:      # If waypoints have already been received
+        if self.base_waypoints:      # If waypoints have already been received
             # Find the closest waypoint to current pos
-            for waypoint in self.waypoints.waypoints:
+            for waypoint in self.base_waypoints.waypoints:
                 distances.append(dist_f(waypoint.pose.pose.position, msg.pose.position))
 
             # Get index of closest waypoint
             self.closest_index = np.argmin(distances)
 
             # We need to add the waypoints in front of us, but we need to consider this is a circular track
-            if self.closest_index + LOOKAHEAD_WPS + 1 > np.shape(self.waypoints.waypoints)[0]:
+            if self.closest_index + LOOKAHEAD_WPS + 1 > np.shape(self.base_waypoints.waypoints)[0]:
                 # We are close to the end of the track, we need to take points from the beginning
                 end_index = self.closest_index + LOOKAHEAD_WPS + 1 - np.shape(self.waypoints.waypoints)[0]
-                self.final_waypoints = self.waypoints.waypoints[self.closest_index:] + self.waypoints.waypoints[:end_index]
+                self.final_waypoints = self.base_waypoints.waypoints[self.closest_index:] + self.base_waypoints.waypoints[:end_index]
             else:
-                self.final_waypoints = self.waypoints.waypoints[self.closest_index:self.closest_index+LOOKAHEAD_WPS+1]
+                self.final_waypoints = self.base_waypoints.waypoints[self.closest_index:self.closest_index+LOOKAHEAD_WPS+1]
 
             # Publish final waypoints
             self.publish_waypoints()
 
     def waypoints_cb(self, waypoints):
         # Storing waypoints given that they are published only once
-        self.waypoints = waypoints
+        self.base_waypoints = waypoints
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
-        pass
+        self.traffic_waypoint = msg.data
 
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message. We will implement it later
-        pass
+        self.obstacle_waypoint = msg.data
 
     def get_waypoint_velocity(self, waypoint):
         return waypoint.twist.twist.linear.x
@@ -91,6 +96,7 @@ class WaypointUpdater(object):
     def set_waypoint_velocity(self, waypoints, waypoint, velocity):
         waypoints[waypoint].twist.twist.linear.x = velocity
 
+    # Note that wp1 and wp2 are indexes
     def distance(self, waypoints, wp1, wp2):
         dist = 0
         dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)

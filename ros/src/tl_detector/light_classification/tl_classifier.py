6d3638d02a6ae37f411ea201ded5f68fb9c15769
Rademacs <rademacs@RadMac.local>
2018-02-14 18:47:12 +0100

    Loading Tensorflow Model
    Loading Tensorflow Model

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,50 @@
 from styx_msgs.msg import TrafficLight
 
+import os
+import sys
+import tensorflow as tf
+
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        pass
+        curr_dir = os.path.dirname(os.path.realpath(__file__))
+
+        model = curr_dir + '/trafficlight_model_sim/frozen_inference_graph.pb'
+
+        path_to_labels = curr_dir + '/label_map.pbtxt'
+        num_classes = 4
+
+        self.detection_graph = tf.Graph()
+
+        #Load frozen Tensorflow model into memory
+        detection_graph = tf.Graph()
+        with detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+
+            self.sess = tf.Session(graph=self.detection_graph, config=config)
+
+        self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+        # Each box represents a part of the image where a particular object was detected.
+        self.boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        self.scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        self.classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+        self.num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+
+        #Loading label map
+        label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+        categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+
+        self.category_index = label_map_util.create_category_index(categories)
+
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -16,4 +57,16 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
+
+        image_expanded = np.expand_dims(image, axis=0)
+        with self.detection_graph.as_default():
+            (boxes, scores, classes, num) = self.sess.run(
+            [self.detection_boxes, self.detection_scores,
+            self.detection_classes, self.num_detections],
+            feed_dict={self.image_tensor: image_expanded})
+
+        boxes = np.squeeze(boxes)
+        scores = np.squeeze(scores)
+        classes = np.squeeze(classes).astype(np.int32)
+
         return TrafficLight.UNKNOWN

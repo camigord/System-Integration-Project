53c53b680aa9e7417fef4d90f53f521b46d354f1
camigord <camigord@gmail.com>
2018-02-23 16:19:04 +0100

    restoring previous bridge file

--- ros/src/styx/bridge.py
+++ ros/src/styx/bridge.py
@@ -17,7 +17,6 @@ import numpy as np
 from PIL import Image as PIL_Image
 from io import BytesIO
 import base64
-import random
 
 import math
 
@@ -176,13 +175,12 @@ class Bridge(object):
         self.publishers['dbw_status'].publish(Bool(data))
 
     def publish_camera(self, data):
-        if random.uniform(0,1) < 0.1:
-            imgString = data["image"]
-            image = PIL_Image.open(BytesIO(base64.b64decode(imgString)))
-            image_array = np.asarray(image)
+        imgString = data["image"]
+        image = PIL_Image.open(BytesIO(base64.b64decode(imgString)))
+        image_array = np.asarray(image)
 
-            image_message = self.bridge.cv2_to_imgmsg(image_array, encoding="rgb8")
-            self.publishers['image'].publish(image_message)
+        image_message = self.bridge.cv2_to_imgmsg(image_array, encoding="rgb8")
+        self.publishers['image'].publish(image_message)
 
     def callback_steering(self, data):
         self.server('steer', data={'steering_angle': str(data.steering_wheel_angle_cmd)})
